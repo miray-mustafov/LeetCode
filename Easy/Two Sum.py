50106e1310674378764dabf90dfd80b72d00d011
miray-mustafov <mustafovmiray@gmail.com>
2023-03-17 20:51:23 +0200

    adding better solution to two sum

--- Easy/Two Sum.py
+++ Easy/Two Sum.py
@@ -1,11 +1,19 @@
+def twoSumOnComplexity(nums, target):
+    d = {}
+    for i, j in enumerate(nums):
+        r = target - j
+        if r in d: return [d[r], i]
+        d[j] = i
+
+
 def twoSum(nums, target):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target and nums[i] is not nums[j]:
+            if nums[i] + nums[j] == target:
                 return [i, j]
 
 
-nums = [2, 7, 11, 15]
+nums = [2, 7, 11, 15, ]
 target = 9
 
 print(twoSum(nums, target))
