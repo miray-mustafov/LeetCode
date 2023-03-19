308710721c8d9749a3135e76856daf48b14f4eea
miray-mustafov <mustafovmiray@gmail.com>
2023-03-19 11:18:43 +0200

    Roman to integer starting point

---
+++ Easy/02.Roman to Integer.py
@@ -0,0 +1,13 @@
+"""
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+"""
+three = 'III'
+five8 = 'LVIII'
+one994 = 'MCMXCIV'
