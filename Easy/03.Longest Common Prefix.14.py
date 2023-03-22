f57c716d1fbba5b24c1380b39513424ba54267aa
miray-mustafov <mustafovmiray@gmail.com>
2023-03-22 09:04:23 +0200

    horizontal scanning longest common prefix

--- Easy/03.Longest Common Prefix.14.py
+++ Easy/03.Longest Common Prefix.14.py
@@ -1,4 +1,5 @@
 def longestCommonPrefix(strs):
+    # Vertical Scanning method
     if strs.__len__() == 1: return strs[0]
 
     prefix = ""
@@ -15,13 +16,29 @@ def longestCommonPrefix(strs):
     return prefix
 
 
-strs = ["flower", "flow", "flight"]  # "fl"
+def horizontal_scanning(strs):
+    prefix = strs[0]
+    for str in strs[1:]:
+        while str[:len(prefix)] != prefix:
+            prefix = prefix[:-1]
+
+            if not prefix:
+                return ''
+    return prefix
+
+
+strs = ["flow", "flower", "flight"]  # "fl"
 strs2 = ["dog", "racecar", "car"]  # ""
 strs3 = ["dog"]  # "dog"
 strs4 = ["cir", "car"]
+
 print(
     longestCommonPrefix(strs),
     longestCommonPrefix(strs2),
     longestCommonPrefix(strs3),
 )
 print(longestCommonPrefix(strs4))
+
+print(horizontal_scanning(strs))
+print(horizontal_scanning(strs3))
+print(horizontal_scanning(strs2))
