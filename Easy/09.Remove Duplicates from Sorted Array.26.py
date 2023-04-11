de2926e288f9eb4b3ffd520258ffa8fde4cadaf5
miray-mustafov <mustafovmiray@gmail.com>
2023-04-11 20:41:01 +0300

    Remove duplicates question 26 leetcode my solo attempt 79%, 50%

---
+++ Easy/09.Remove Duplicates from Sorted Array.26.py
@@ -0,0 +1,15 @@
+def removeDuplicates(nums):
+    curr_i = 0
+    for i in range(1, len(nums)):
+        if nums[curr_i] == nums[i]:
+            nums[i] = '_'
+        else:
+            curr_i += 1
+            nums[curr_i], nums[i] = nums[i], nums[curr_i]
+    return curr_i + 1
+
+
+nums = [1, 1, 2]  # [1,2,_]
+nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]  # [0,1,2,3,4,_,_,_,_,_]
+
+print('k=', removeDuplicates(nums))
