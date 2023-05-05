a4ec35249d194129e956057680ecaf4143961f13
miray-mustafov <mustafovmiray@gmail.com>
2023-05-05 14:18:14 +0300

    26. Remove Duplicates from Sorted Array my first attempt

--- ArrayString/02.27. Remove Element.py
+++ ArrayString/02.27. Remove Element.py
@@ -1,19 +1,17 @@
 def removeElement(nums, val):
-    k = 0
     l = 0
     r = len(nums) - 1
     while l <= r:
         if nums[r] == val:
             nums[r], r = '_', r - 1
-            k += 1
         else:
             if nums[l] == val:
                 nums[l] = '_'
                 nums[l], nums[r] = nums[r], nums[l]
-                r, l, k = r - 1, l + 1, k + 1
+                r, l = r - 1, l + 1
             else:
                 l += 1
-    return len(nums) - k
+    return l
 
 
 nums = [0, 1, 2, 2, 3, 0, 4, 2]
