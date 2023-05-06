7046e0ab66ccbca440a71537428067c2396cef8e
miray-mustafov <mustafovmiray@gmail.com>
2023-05-06 20:29:09 +0300

    169. Majority Element hashmap solution

--- ArrayString/05.169. Majority Element.py
+++ ArrayString/05.169. Majority Element.py
@@ -1,2 +1,15 @@
 def majorityElement(nums):
-    occurance_border = len(nums)
+    occurances = {}
+    res, max_count = 0, 0
+    for n in nums:
+        occurances[n] = 1 + occurances.get(n, 0)
+        if occurances[n] > max_count:
+            res = n
+        max_count = max(max_count, occurances[n])
+    return res
+
+
+
+
+nums = [2, 2, 1, 1, 1, 2, 2,3]
+majorityElement(nums)
