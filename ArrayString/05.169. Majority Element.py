d684b84c6269171635e4ff05ed9844d1efb9971d
miray-mustafov <mustafovmiray@gmail.com>
2023-05-07 10:03:41 +0300

    189. Rotate Array

--- ArrayString/05.169. Majority Element.py
+++ ArrayString/05.169. Majority Element.py
@@ -9,7 +9,17 @@ def majorityElement(nums):
     return res
 
 
+def mespaceefficient(nums):
+    count = 0
+    res = 0
+    for n in nums:
+        if count == 0:
+            res = n
+        count += (1 if n == res else -1)
+    return res
 
 
-nums = [2, 2, 1, 1, 1, 2, 2,3]
-majorityElement(nums)
+nums = [2, 2, 1, 1, 3, 2, 2]
+nums2 = [3, 2, 3]
+print(mespaceefficient(nums))
+print(mespaceefficient(nums2))
---
+++ ArrayString/06.189. Rotate Array.py
@@ -0,0 +1,29 @@
+def rotate_array(nums, k):
+    """
+    Rotates the given array to the right by k steps in place.
+    """
+    n = len(nums)
+    k %= n
+    nums[:] = nums[n - k:] + nums[:n - k]
+    print(nums)
+
+
+def rotate(nums, k):
+    def reverse_subarray(arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start, end = start + 1, end - 1
+
+    n = len(nums)
+    k = k % n
+    reverse_subarray(nums, 0, n - 1)
+    reverse_subarray(nums, 0, k - 1)
+    reverse_subarray(nums, k, n - 1)
+    print(nums)
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+nums2 = [1, 2, 3]  # [3,1,2] [2,3,1] [1,2,3]
+k = 3
+rotate_array(nums.copy(), k)
+rotate(nums.copy(), k)
+# [5,6,7,1,2,3,4]
