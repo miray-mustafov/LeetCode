020b6f7ac92afdd2d1ae566ff26a949a9495532a
miray-mustafov <mustafovmiray@gmail.com>
2023-05-08 21:44:52 +0300

    55. Jump Game greedy approach li my jump

--- 09.55. Jump Game.py
+++ 09.55. Jump Game.py
@@ -1,4 +1,4 @@
-def can_jump(nums):
+def my_can_jump(nums):
     if len(nums) == 1:
         return True
     elif nums[0] == 0:
@@ -23,10 +23,32 @@ def can_jump(nums):
     return True
 
 
+def can_jump_dp(nums):  # but fails here [3, 0, 0, 0]
+    statusPosition = [False for _ in range(len(nums))]
+    statusPosition[0] = True
+    i = 0
+    for maxReach in range(1, len(nums)):
+        while i < maxReach:
+            if statusPosition[i] is True and nums[i] + i >= maxReach:
+                statusPosition[maxReach] = True
+                break
+            i += 1
+
+    return statusPosition[-1]
+
+
+def can_jump_greedy(nums):
+    goal = len(nums) - 1
+    for i in range(len(nums) - 1, -1, -1):
+        if nums[i] + i >= goal:
+            goal = i
+    return True if goal == 0 else False
+
+
 nums_true = [2, 3, 1, 1, 4]
 nums_true2 = [6, 4, 2, 1, 0, 4]
 nums_true3 = [2, 0, 4, 2, 1, 0, 1, 4]
 nums_false = [2, 0, 3, 2, 1, 0, 4]
 nums_false2 = [1, 0, 3, 2, 0, 4]
-nums_false3 = [3,0,0,0]
-print(can_jump(nums_false))
+nums_false3 = [3, 0, 0, 0]
+print(can_jump_greedy(nums_false3))
