4a819ec57b1a996d321e5ecd757b3abc464dee4d
miray-mustafov <mustafovmiray@gmail.com>
2023-05-09 20:00:10 +0300

    275. H-Index II

--- ArrayString/12.H- index II.py
+++ ArrayString/12.H- index II.py
@@ -1,13 +1,26 @@
-nums = [0, 1, 6, 7, 9, 7, 12]
-n = len(nums)
-print(n)
-print(n - 2)
-
-
-def hIndex_not_logn(nums):
-    n = len(nums)
+def hIndex(citations):
+    n = len(citations)
     for i in range(n):
         diff = n - i
-        if nums[i] >= diff:
+        if citations[i] >= diff:
             return diff
+    return 0
 
+
+def hIndex_logn(citations):
+    if not citations or citations[-1] == 0:
+        return 0
+    N = len(citations)
+    l, r = 0, N - 1
+    while l < r:
+        m = (l + r) // 2
+        if N - m > citations[m]:
+            l = m + 1
+        else:
+            r = m
+    return N - l
+
+
+nums = [0, 1, 2, 6, 7, 9, 7, 12]
+print(hIndex(nums))
+print(hIndex_logn(nums))
