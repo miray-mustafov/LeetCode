5c94c59dc1b1f3147a8cf188bc3d24ce17fa32f9
miray-mustafov <mustafovmiray@gmail.com>
2023-05-09 12:19:38 +0300

    45. Jump Game II

---
+++ ArrayString/09.45. Jump Game II.py
@@ -0,0 +1,20 @@
+def jump(nums):
+    res = 0
+    l, r = 0, 0
+    while r < len(nums) - 1:
+        farthest = 0
+        for i in range(l, r + 1):
+            farthest = max(farthest, nums[i] + i)
+        l = r + 1
+        r = farthest
+        res += 1
+    return res
+
+
+def jump_dp(nums, num, i, jumps):
+    pass
+
+
+nums = [2, 3, 1, 1, 4]
+print(jump(nums))
+# print(jump_dp(nums, nums[0], 0, 0))
