67af01712510b517079f9c45215d2dbec818e038
miray-mustafov <mustafovmiray@gmail.com>
2023-05-10 21:27:36 +0300

    12. Integer to Roman the tutorial clever way

--- ArrayString/17.12. Integer to Roman.py
+++ ArrayString/17.12. Integer to Roman.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 
-def to_roman(num):
+def to_roman(num):  # Super solo
     ROMANS = {
         1: "I",
         5: "V",
@@ -41,8 +41,21 @@ def to_roman(num):
     return ''.join(roman)
 
 
-print(to_roman(1994))
-print(to_roman(58))
+def tutorial(num):
+    symList = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10],
+               ["XL", 40], ["L", 50], ["XC", 90], ["C", 100],
+               ["CD", 400], ["D", 500], ["CM", 900], ["M", 1000]]
+    res = ""
+    for sym, val in reversed(symList):
+        if num // val:
+            count = num // val
+            res += (sym * count)
+            num = num % val
+    return res
+
+
+print(tutorial(1994))
+print(tutorial(58))
 print(to_roman(10))
 print(to_roman(20))
 print(to_roman(30))
