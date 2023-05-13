742e71999eb56dfcb9623b05ebfc7ca7cb60dc15
miray-mustafov <mustafovmiray@gmail.com>
2023-05-13 16:57:04 +0300

    Two sum sorted

--- ArrayString/22.392. Is Subsequence.py
+++ ArrayString/22.392. Is Subsequence.py
@@ -1,18 +1,33 @@
 def f(s, t):
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+
+    return True if i == len(s) else False
+
+
+def f2(s, t):
     if s == '':
         return True
-    i = 0
-    for el in t:
-        if i == len(s):
-            return True
-        if s[i] == el:
-            i += 1
+    if len(s) == 1:
+        for el in t:
+            if s == el: return True
+        return False
 
-    if i == len(s):
+    l, r, i, j = 0, len(t) - 1, 0, len(s) - 1
+    while l < r and i <= j:
+        if s[i] == t[l]:
+            i += 1
+        if s[j] == t[r]:
+            j -= 1
+        l, r = l + 1, r - 1
+    if i > j:
         return True
     return False
 
 
-s = 'abc'
+s = 'c'
 t = 'ahbdgc'
-print(f(s, t))
+print(f2(s, t))
--- ArrayString/23.167. Two Sum II - Input Array Is Sorted.py
+++ ArrayString/23.167. Two Sum II - Input Array Is Sorted.py
@@ -1,12 +1,13 @@
 def f(nums, target):
     l, r = 0, len(nums) - 1
     while l < r:
-        if nums[l] + nums[r] == target:
-            return [l + 1, r + 1]
-        elif nums[l] + nums[r] > target:
+        curr_sum = nums[l] + nums[r]
+        if curr_sum < target:
+            l += 1
+        elif curr_sum > target:
             r -= 1
         else:
-            l += 1
+            return [l + 1, r + 1]
 
 
 nums = [1, 3, 4, 5, 7, 11, 11]
