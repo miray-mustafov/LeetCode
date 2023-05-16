8920c0e4454337334647f369a381d98df31370a5
miray-mustafov <mustafovmiray@gmail.com>
2023-05-16 21:28:12 +0300

    209. Minimum Size Subarray Sum

--- ArrayString/26.209. Minimum Size Subarray Sum.py
+++ ArrayString/26.209. Minimum Size Subarray Sum.py
@@ -1,18 +1,20 @@
 def f(target, nums):
-    windows = []
-    suma = 0
-    l = 0
-    for r in range(len(nums)):
-        suma += nums[r]
-        if suma > target:
-            l = r
-            suma = 0
-        elif suma == target:
-            windows.append(r - l)
+    l, r, total = 0, 0, 0
+    res = float('inf')
+    while r < len(nums):
+        total += nums[r]
+        while total >= target:
+            res = min(r - l + 1, res)
+            total -= nums[l]
+            l += 1
+        r += 1
 
-    return min(windows)
+    return 0 if res == float('inf') else res
 
 
 target = 7
 nums = [2, 3, 1, 2, 4, 3]
 print(f(target, nums))
+print(f(11, [1, 2, 3, 4, 5]))
+print(f(6, [2, 2, 2, 2, 4]))
+print(f(6, [2, 2, 3, 3]))
