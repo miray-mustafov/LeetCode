9a363c22d72e5cec5aa5f66f8cfe7e490275a84b
miray-mustafov <mustafovmiray@gmail.com>
2023-05-22 16:29:28 +0300

    49. Group Anagrams

--- 05.HashMap/05.49. Group Anagrams.py
+++ 05.HashMap/05.49. Group Anagrams.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
+
+
 def groupAnagrams(strs):
-    from collections import defaultdict
     def orderByAscii(string):
         ordered_chars = sorted(string, key=lambda x: ord(x))
         return ''.join(ordered_chars)
@@ -12,6 +14,22 @@ def groupAnagrams(strs):
     return res
 
 
+def groupAnagrams2(strs):
+    def count_chars(s):
+        char_count = [0] * 26  # Assuming lowercase English letters
+        for char in s:
+            char_count[ord(char) - ord('a')] += 1
+        return tuple(char_count)
+
+    grouped_anagrams = defaultdict(list)
+    for s in strs:
+        char_count = count_chars(s)
+        grouped_anagrams[char_count].append(s)
+
+    return list(grouped_anagrams.values())
+
+
 strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 
 print(groupAnagrams(strs))
+print(groupAnagrams2(strs))
