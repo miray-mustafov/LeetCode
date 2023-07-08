bfd8eb54382449a287b5f0fbd49eed43f9a140d2
miray-mustafov <mustafovmiray@gmail.com>
2023-07-07 20:16:53 -0500

    104. Maximum Depth of Binary Tree

---
+++ 08.Linked List/10.146. LRU Cache.py
@@ -0,0 +1,14 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+
+    def get(self, key: int) -> int:
+
+    def put(self, key: int, value: int) -> None:
+
+# Your LRUCache object will be instantiated and called as such:
+capacity = 2
+key = 1
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
