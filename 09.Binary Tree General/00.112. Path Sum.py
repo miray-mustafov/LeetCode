7b5463d904a58c47f3b859732e5b81357dd3ab6a
miray-mustafov <mustafovmiray@gmail.com>
2023-07-13 21:02:21 -0500

    112. Path Sum

---
+++ 09.Binary Tree General/00.112. Path Sum.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def hasPathSum(root, targetSum):
+    def dfs(node, cur):
+        if not node:
+            return False
+        cur += node.val
+        if not node.left and not node.right:
+            return cur == targetSum
+
+        return dfs(node.left, cur) or dfs(node.right, cur)
+
+    return dfs(root, 0)
