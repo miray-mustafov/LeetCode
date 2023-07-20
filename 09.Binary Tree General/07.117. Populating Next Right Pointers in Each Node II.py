4a42e6648e16d1202001574d3a8e2ed60ff35c12
miray-mustafov <mustafovmiray@gmail.com>
2023-07-20 16:59:21 -0500

    117. Populating Next Right Pointers in Each Node II

--- 09.Binary Tree General/07.117. Populating Next Right Pointers in Each Node II.py
+++ 09.Binary Tree General/07.117. Populating Next Right Pointers in Each Node II.py
@@ -8,4 +8,17 @@ class Node:
 
 
 class Solution:
-    def connect(self, root: 'Node') -> 'Node':
+    def connect(self, root):
+        cur, nxt = root, root.left if root else None
+
+        while cur and nxt:
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
+
+            cur = cur.next
+            if not cur:
+                cur = nxt
+                nxt = cur.left
+        return root
+
