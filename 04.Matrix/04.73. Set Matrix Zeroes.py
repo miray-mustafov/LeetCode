5c50378b276a260a2d29da44c92edb2bfc983c6b
miray-mustafov <mustafovmiray@gmail.com>
2023-08-20 15:07:51 -0500

    startup space mn solution 73. Set Matrix Zeroes

---
+++ 04.Matrix/04.73. Set Matrix Zeroes.py
@@ -0,0 +1,22 @@
+def setZeroes(matrix):
+    c = []
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] == 0:
+                c.append((row, col))
+    for cor in c:
+        for col in range(len(matrix[0])):
+            matrix[cor[0]][col] = 0
+        for row in range(len(matrix)):
+            matrix[row][cor[1]] = 0
+
+    [print(row) for row in matrix]
+
+
+matrix = [
+    [5, 0, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 0, 7],
+    [15, 5, 2, 6]
+]
+setZeroes(matrix)
