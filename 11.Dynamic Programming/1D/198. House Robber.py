2f75bff2e5ab790cf984825c2bfcb32f1ebc44cb
miray-mustafov <mustafovmiray@gmail.com>
2023-11-22 19:49:05 +0200

    198. House Robber

--- 11.Dynamic Programming/1D/198. House Robber.py
+++ 11.Dynamic Programming/1D/198. House Robber.py
@@ -6,6 +6,16 @@ def rob(nums):
     return max(dp[-1], dp[-2])
 
 
+def robTutorial(nums):
+    rob1, rob2 = 0, 0
+    for n in nums:
+        temp = max(rob1 + n, rob2)
+        rob1 = rob2
+        rob2 = temp
+    return rob2
+
+
 nums = [1, 2, 3, 1]
 # Output: 4
 print(rob(nums))
+print(robTutorial(nums))
