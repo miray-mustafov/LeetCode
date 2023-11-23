ee3765bbe03a599d72b0e89711d9433c9adf5dc4
miray-mustafov <mustafovmiray@gmail.com>
2023-11-23 12:28:10 +0200

    91. decode ways 0(1) space compl

--- 11.Dynamic Programming/1D/91. Decode Ways.py
+++ 11.Dynamic Programming/1D/91. Decode Ways.py
@@ -1,4 +1,4 @@
-def numDecodingsDP(s):
+def numDecodingsDP(s):  # 0(n) space
     dp = [0] * (len(s) + 1)
     dp[len(s)] = 1
     for i in range(len(s) - 1, -1, -1):
@@ -13,4 +13,19 @@ def numDecodingsDP(s):
     return dp[0]
 
 
+def numDecodingsDP2(s):  # 0(1) space
+    current, nextt, after_next = 0, 1, 1
+    for i in range(len(s) - 1, -1, -1):
+        current = 0
+        if s[i] != "0":
+            current = nextt
+        if i + 1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i + 1] in '0123456'):
+            current += after_next
+
+        after_next = nextt
+        nextt = current
+    return current
+
+
 print(numDecodingsDP('11106'))  # 2
+print(numDecodingsDP2('11106'))  # 2
