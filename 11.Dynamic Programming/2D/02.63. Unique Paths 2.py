6d6f03dac974aa5537b7f60d7748d81e3c3eef2f
miray-mustafov <mustafovmiray@gmail.com>
2023-11-24 13:43:52 +0200

    unique Paths without reversing also implemented

--- 11.Dynamic Programming/2D/02.63. Unique Paths 2.py
+++ 11.Dynamic Programming/2D/02.63. Unique Paths 2.py
@@ -14,7 +14,7 @@ def uniquePathsIIadjustedInput(obstacleGrid):
 # print(uniquePathsIIadjustedInput(grid))
 
 
-def uniquePathsII(grid):  # time Om+n space On
+def uniquePathsIIreversed(grid):  # time Om+n space On
     m, n = len(grid), len(grid[0])
     dp = [0] * n
     dp[n - 1] = 1
@@ -27,7 +27,21 @@ def uniquePathsII(grid):  # time Om+n space On
     return dp[0]
 
 
-def uniquePathsIItable(grid):
+def uniquePathsII(grid):  # time Om+n space On
+    m, n = len(grid), len(grid[0])
+    dp = [0] * n
+    dp[0] = 1
+    for r in range(m):
+        for c in range(n):
+            if grid[r][c]:
+                dp[c] = 0
+            elif c - 1 > -1:
+                dp[c] = dp[c] + dp[c - 1]
+    return dp[n - 1]
+
+
+# -----------------------------------------------------------------------
+def uniquePathsIItableReversed(grid):
     m, n = len(grid), len(grid[0])
     dp = [[0] * (n + 1) for _ in range(m + 1)]
     dp[m - 1][n - 1] = 1
@@ -40,7 +54,21 @@ def uniquePathsIItable(grid):
     return dp[0][0]
 
 
-grid = [[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]]
+def uniquePathsIItable(grid):
+    m, n = len(grid), len(grid[0])
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    dp[1][1] = 1
+    for r in range(1, m + 1):
+        for c in range(1, n + 1):
+            if grid[r - 1][c - 1]:
+                dp[r][c] = 0
+            else:
+                dp[r][c] += dp[r - 1][c] + dp[r][c - 1]
+    return dp[m][n]
+
+
+# grid4 = [[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]]  # output: 4
+grid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
 grid2 = [[0, 1], [0, 0]]
 grid3 = [[0, 1], [1, 0]]
 print(uniquePathsIItable(grid))
