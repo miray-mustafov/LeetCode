3b3bc7b76f4906035149ff3c6c75c40c3486be01
miray-mustafov <mustafovmiray@gmail.com>
2023-11-24 12:12:13 +0200

    unique paths II from end to 00 approach

--- 11.Dynamic Programming/2D/02.63. Unique Paths 2.py
+++ 11.Dynamic Programming/2D/02.63. Unique Paths 2.py
@@ -1,4 +1,4 @@
-def uniquePathsII(obstacleGrid):  # space O(m*n)
+def uniquePathsIIadjustedInput(obstacleGrid):
     m, n = len(obstacleGrid), len(obstacleGrid[0])
     for i in range(1, m):
         for j in range(1, n):
@@ -9,7 +9,27 @@ def uniquePathsII(obstacleGrid):  # space O(m*n)
     return obstacleGrid[m - 1][n - 1]
 
 
-grid = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
-grid2 = [[1, 0], [1, 1]]
+# grid = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+# grid2 = [[1, 0], [1, 1]]
+# print(uniquePathsIIadjustedInput(grid))
+
+
+def uniquePathsII(grid):
+    m, n = len(grid), len(grid[0])
+    dp = [0] * n
+    dp[n - 1] = 1
+    for r in reversed(range(m)):
+        for c in reversed(range(n)):
+            if grid[r][c]:
+                dp[c] = 0
+            elif c + 1 < n:
+                dp[c] = dp[c] + dp[c + 1]
+    return dp[0]
+
+
+grid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+grid2 = [[0, 1], [0, 0]]
+grid3 = [[0, 1], [1, 0]]
 print(uniquePathsII(grid))
 print(uniquePathsII(grid2))
+print(uniquePathsII(grid3))
