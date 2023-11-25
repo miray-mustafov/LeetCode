0d8fe055c9ded9e46d46b34a04ed8c65815c6b7c
miray-mustafov <mustafovmiray@gmail.com>
2023-11-25 18:42:33 +0200

    lcs opposite symmetrical approach

--- 11.Dynamic Programming/2D/03.1143. Longest Common Subsequence.py
+++ 11.Dynamic Programming/2D/03.1143. Longest Common Subsequence.py
@@ -1,4 +1,4 @@
-def lcs(text1, text2):
+def lcsUp(text1, text2):
     m, n = len(text1), len(text2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
     for i in range(m - 1, -1, -1):
@@ -10,6 +10,19 @@ def lcs(text1, text2):
     return dp[0][0]
 
 
+def lcsDown(text1, text2):
+    m, n = len(text1), len(text2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+    return dp[m][n]
+
+
 text1 = "abcde"
 text2 = "ace"
-print(lcs(text1, text2))  # 3 ace
+print(lcsUp(text1, text2))  # 3 ace
+print(lcsDown(text1, text2))  # 3 ace
