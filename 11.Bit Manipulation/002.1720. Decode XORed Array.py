ef7d933a38f268ade3fd3be4c522036287b696cc
miray-mustafov <mustafovmiray@gmail.com>
2023-11-27 23:32:08 +0200

    decode 2

--- 11.Bit Manipulation/002.1720. Decode XORed Array.py
+++ 11.Bit Manipulation/002.1720. Decode XORed Array.py
@@ -7,7 +7,17 @@ def decode(encoded, first):
     return encoded
 
 
+def decode2(encoded, first):
+    out = [first]
+    n = len(encoded)
+    for i in range(n):
+        out.append(out[i] ^ encoded[i])
+    return out
+
+
 encoded = [1, 2, 3]
 first = 1
 # Output: [1,0,2,1]
+encoded2 = [1, 2, 3]
 print(decode(encoded, first))
+print(decode2(encoded2, first))
