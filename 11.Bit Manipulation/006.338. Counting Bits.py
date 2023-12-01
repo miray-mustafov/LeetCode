24c749b83cab54156d3e5195d629ebb6c792c105
miray-mustafov <mustafovmiray@gmail.com>
2023-12-01 09:11:57 +0200

    338 Counting bits DP solution On

--- 11.Bit Manipulation/006.338. Counting Bits.py
+++ 11.Bit Manipulation/006.338. Counting Bits.py
@@ -12,6 +12,17 @@ def countBits(n):
     return arr
 
 
-n = 5
+def countBits2(n):
+    dp = [0] * (n + 1)
+    offset = 1
+    for i in range(1, len(dp)):
+        if offset * 2 == i:
+            offset = i
+        dp[i] = 1 + dp[i - offset]
+    return dp
+
+
+n = 10
 # Output: [0,1,1,2,1,2]
 print(countBits(n))
+print(countBits2(n))
