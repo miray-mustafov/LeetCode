54ba4361bb66c7241dce31a9a690c447d45bd34b
miray-mustafov <mustafovmiray@gmail.com>
2023-12-11 15:10:09 +0200

    135. Candy

--- 01.ArrayString/23.135. Candy.py
+++ 01.ArrayString/23.135. Candy.py
@@ -28,11 +28,33 @@ def candy2(ratings):
     return sum(arr)
 
 
+def candy3(ratings):
+    if not ratings:
+        return 0
+
+    ret, up, down, peak = 1, 0, 0, 0
+
+    for prev, curr in zip(ratings[:-1], ratings[1:]):
+        if prev < curr:
+            up, down, peak = up + 1, 0, up + 1
+            ret += 1 + up
+        elif prev == curr:
+            up = down = peak = 0
+            ret += 1
+        else:
+            up, down = 0, down + 1
+            ret += 1 + down - int(peak >= down)
+
+    return ret
+
+
 r1 = [1, 0, 2]  # 5
 r2 = [1, 2, 2]  # 4
-r3 = [0, 2, 3, 2, 1]  # 1 2 3 2 1
+r3 = [0, 2, 3, 2, 1]  # 1 2 3 2 1 = 9
+r5 = [0, 2, 3, 2, 1, 0, -1]  # 1 2 5 4 3 2 1 = 9
 r4 = [1, 2, 0, 1]  # 1, 2 ,1 ,2 = 6
 print(candy2(r1))
+print(candy3(r5))
 # print(candy(r1))
 # print(candy(r2))
 # print(candy(r3))
