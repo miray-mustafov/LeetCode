964c45e6dc2154f40434e212bf074b1965e1402e
miray-mustafov <mustafovmiray@gmail.com>
2023-12-11 14:24:12 +0200

    135. Candy

--- 01.ArrayString/23.135. Candy.py
+++ 01.ArrayString/23.135. Candy.py
@@ -1,4 +1,4 @@
-def candy(ratings):
+def candy(ratings):  # My try for O(1*N) solution but unsuccessful
     min_candy = 1
     slider_dec, slider_inc = 0, 0
     for i in range(1, len(ratings)):
@@ -15,10 +15,25 @@ def candy(ratings):
     return min_candy
 
 
+def candy2(ratings):
+    arr = [1] * len(ratings)
+    for i in range(1, len(ratings)):
+        if ratings[i - 1] < ratings[i]:
+            arr[i] = arr[i - 1] + 1
+
+    for i in range(len(ratings) - 2, -1, -1):
+        if ratings[i] > ratings[i + 1]:
+            arr[i] = max(arr[i], arr[i + 1] + 1)
+
+    return sum(arr)
+
+
 r1 = [1, 0, 2]  # 5
 r2 = [1, 2, 2]  # 4
-r3=[0,2,3,2,1]# 1 2 3 2 1
-print(candy(r1))
-print(candy(r2))
-print(candy(r3
-            ))
+r3 = [0, 2, 3, 2, 1]  # 1 2 3 2 1
+r4 = [1, 2, 0, 1]  # 1, 2 ,1 ,2 = 6
+print(candy2(r1))
+# print(candy(r1))
+# print(candy(r2))
+# print(candy(r3))
+# print(candy(r4))
