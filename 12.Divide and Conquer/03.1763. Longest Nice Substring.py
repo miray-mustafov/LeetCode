b8d344474f845f1df0cc8a113c931738b265f17c
miray-mustafov <mustafovmiray@gmail.com>
2023-12-12 21:23:57 +0200

    longest nice sub divide and conquor try

--- 12.Divide and Conquer/03.1763. Longest Nice Substring.py
+++ 12.Divide and Conquer/03.1763. Longest Nice Substring.py
@@ -1,4 +1,4 @@
-def longestNiceSubstring(s):
+def longestNiceSubstring(s):  # On^3
     N, res = len(s), ''
 
     def is_nice(start, end):
@@ -15,6 +15,24 @@ def longestNiceSubstring(s):
     return res
 
 
+def longestNiceSubstring2(s):  # Onlogn
+    if len(s) < 2:
+        return ""
+
+    def is_nice(sub):
+        return all(ch.swapcase() in sub for ch in sub)
+
+    mid = len(s) // 2
+
+    left_substring = longestNiceSubstring(s[:mid])
+    right_substring = longestNiceSubstring(s[mid:])
+
+    if is_nice(s):
+        return s
+
+    return max(left_substring, right_substring, key=len)
+
+
 s = 'YazaAay'
 s2 = 'YayaAaz'
 s3 = "Bb"
@@ -23,3 +41,8 @@ print(longestNiceSubstring(s))
 print(longestNiceSubstring(s2))
 print(longestNiceSubstring(s3))
 print(longestNiceSubstring(s4))
+print()
+print(longestNiceSubstring2(s))
+print(longestNiceSubstring2(s2))
+print(longestNiceSubstring2(s3))
+print(longestNiceSubstring2(s4))
