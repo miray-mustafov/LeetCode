6d75df29f633763f6237ec767b44aeff4d677a7e
miray-mustafov <mustafovmiray@gmail.com>
2023-12-18 13:33:24 +0200

    Kadane's Algorithm 53. Maximum Subarray

--- 17.Kadane's Algorithm/01.53. Maximum Subarray.py
+++ 17.Kadane's Algorithm/01.53. Maximum Subarray.py
@@ -1,3 +1,4 @@
+# Kadane's Algorithm involves Dynamic programming and greedy approaches
 def maxSubArray(nums):
     maxSum = nums[0]
     curSum = 0
@@ -10,7 +11,18 @@ def maxSubArray(nums):
     return maxSum
 
 
+def maxSubArray2(nums):
+    maxSum = curSum = nums[0]
+
+    for num in nums[1:]:
+        curSum = max(num, curSum + num)
+        maxSum = max(maxSum, curSum)
+
+    return maxSum
+
+
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]  # output 6
 numsNegative = [-2, -2, -1, -5, -4]  # -1
 print(maxSubArray(nums))
+print(maxSubArray2(nums))
 print(maxSubArray(numsNegative))
