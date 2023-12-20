6b6aebf025f058051f1644adb26753adc5fc08a5
miray-mustafov <mustafovmiray@gmail.com>
2023-12-21 01:31:39 +0200

    memo

--- 14.Dynamic Programming/1D/dividecorridor.py
+++ 14.Dynamic Programming/1D/dividecorridor.py
@@ -1,25 +1,34 @@
 # 2147. Number of Ways to Divide a Long Corridor
 def numberOfWays(corridor):
-    def dfs(i, seats):
+    memo = [[-1] * 3 for _ in range(len(corridor))]  # 1.memoization declaration
+    modulo = 10 ** 9 + 7
+
+    def bfa(i, s):
         if i == len(corridor):
-            return 1 if seats == 2 else 0
+            return 1 if s == 2 else 0
+        if memo[i][s] != -1:  # 2.memoization suspend repeated work
+            return memo[i][s]
 
         res = 0
-        if seats == 2:
+        if s == 2:
             if corridor[i] == 'S':
-                res = dfs(i + 1, 1)
+                res = bfa(i + 1, 1)
             else:
-                res = dfs(i + 1, 0) + dfs(i + 1, 2)
+                res = (bfa(i + 1, 0) + bfa(i + 1, 2)) % modulo
         else:
             if corridor[i] == 'S':
-                res = dfs(i + 1, seats + 1)
+                res = bfa(i + 1, s + 1)
             else:
-                res = dfs(i + 1, seats)
+                res = bfa(i + 1, s)
 
+        memo[i][s] = res  # 3.fill memoization dict
         return res
 
-    return dfs(0, 0)
+    return bfa(0, 0)
 
 
-corridor = "SSPPSPSPSS"  # 3
+corridor = "SSPPSPS"  # 3
+corridor2 = "PPPPPSPPSPPSPPPSPPPPSPPPPSPPPPSPPSPPPSPSPPPSPSPPPSPSPPPSPSPPPPSPPPPSPPPSPPSPPPPSPSPPPPSPSPPPPSPSPPPSPPSPPPPSPSPSS"
+
 print(numberOfWays(corridor))
+print(numberOfWays(corridor2))  # 919999993 (after theh %10^9 + 7)
