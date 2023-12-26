ec053c9adaedb80d3fb3495a689fafda4bb28f7f
miray-mustafov <mustafovmiray@gmail.com>
2023-12-26 22:48:44 +0200

    maxSlidingWindow with error

--- 03.SlidingWindow/hard/01.239. Sliding Window Maximum.py
+++ 03.SlidingWindow/hard/01.239. Sliding Window Maximum.py
@@ -1,12 +1,39 @@
+import collections
+
+
 def maxSlidingWindow(nums, k):
     res = []
-    for i in range(len(nums)-(k-1)):
+    for i in range(len(nums) - (k - 1)):
         cur_max = nums[i]
         for j in range(i, i + k):
             cur_max = max(nums[j], cur_max)
         res.append(cur_max)
     return res
 
-nums = [1, 3, -1, -3, 5, 3, 6, 7]  # Output: [3,3,5,5,6,7]
+#
+# def solution(nums, k):
+#     output = []
+#     q = collections.deque()
+#     l = r = 0
+#     while r < len(nums):
+#         while q and nums[q[-1]] < nums[r]:
+#             q.pop()
+#         q.append(r)
+#         if l < q[0]:
+#             q.popleft()
+#         if (r + 1) >= k:
+#             output.append(nums[q[0]])
+#             l += 1
+#         r += 1
+#
+#     return output
+
+
+nums = [3, 1, -1, -3, -4, 5, 3, 6, 7]
 k = 3
-print(maxSlidingWindow(nums, k))
+nums2 = [1, 2, 0, 1, 9, 4, 2, 1, 4, 6, 7, 2]
+k2 = 5
+nums3 = [7, 6, 5, 4, 3, 2, 1, 0]  # Output: [3,3,5,5,6,7]
+
+# print(solution(nums, k))
+# print(solution(nums3, k))
