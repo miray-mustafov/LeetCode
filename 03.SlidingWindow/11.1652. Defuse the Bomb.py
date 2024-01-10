d9a6476c29b6b80733ee1d5f9e89d799d86d80b9
miray-mustafov <mustafovmiray@gmail.com>
2024-01-10 17:25:07 +0200

    1652. Defuse the Bomb

---
+++ 03.SlidingWindow/11.1652. Defuse the Bomb.py
@@ -0,0 +1,30 @@
+def decrypt(code, k):
+    n = len(code)
+    if k == 0:
+        return [0] * n
+
+    # todo def helper()
+    cur_sum, res = 0, []
+    if k > 0:
+        for i in range(1, k + 1):  # can optimize for abs(k)>n
+            cur_sum += code[i % n]
+        res.append(cur_sum)
+        for i in range(1, n):
+            cur_sum = cur_sum - code[i] + code[(i + k) % n]
+            res.append(cur_sum)
+    else:
+        for i in range(n - 1, -(k + 1), -1):  # can optimize for abs(k)>n
+            cur_sum += code[i % n]
+        res.append(cur_sum)
+        for i in range(1, n):
+            cur_sum = cur_sum - code[(i - 1 + k) % n] + code[i - 1]
+            res.append(cur_sum)
+    return res
+
+
+code, k = [5, 7, 1, 4], 3  # Output: [12, 10, 16, 13]
+code2, k2 = [1, 2, 3, 4], 0  # output [0,0,0,0]
+code3, k3 = [2, 4, 9, 3], -2  # Output: [12, 5, 6, 13]
+print(decrypt(code, k))
+print(decrypt(code2, k2))
+print(decrypt(code3, k3))
