2a905772da04cc7e9e3c9d420ba79fe78bd94721
miray-mustafov <mustafovmiray@gmail.com>
2024-02-01 13:00:54 +0200

    1493. Longest Subarray of 1's After Deleting One Element

---
+++ 03.SlidingWindow/13.1493. Longest Subarray of 1's After Deleting One Element.py
@@ -0,0 +1,19 @@
+def sol(nums):
+    l = res = zeroes = 0
+    for r in range(len(nums)):
+        zeroes += 1 if nums[r] == 0 else 0
+        while zeroes > 1:
+            zeroes -= 1 if nums[l] == 0 else 0
+            l += 1
+        res = max(res, r - l)
+    return res
+
+    #   0  1  2  3  4  5  6  7  8  9  10
+
+
+nums = [0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1]  # 5
+print(sol(nums))
+nums = [0, 0, 0]  # 0
+print(sol(nums))
+nums = [1, 0, 0]  # 1
+print(sol(nums))
