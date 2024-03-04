b7cb0c88fec73ba4f94193953784bc3c5a0837c4
miray-mustafov <mustafovmiray@gmail.com>
2024-03-04 11:05:28 +0200

    1031. Maximum Sum of Two Non-Overlapping Subarrays the sliding window approach

--- 03.SlidingWindow/1031. Maximum Sum of Two Non-Overlapping Subarrays.py
+++ 03.SlidingWindow/1031. Maximum Sum of Two Non-Overlapping Subarrays.py
@@ -1,64 +1,4 @@
-def maxSumTwoNoOverlap(nums, firstLen, secondLen):
-    n = len(nums)
-    maxSumFirst = [0] * n
-    maxSumSecond = [0] * n
-
-    # Precompute maximum sum of subarrays of length firstLen
-    currSum = sum(nums[:firstLen])
-    maxSumFirst[firstLen - 1] = currSum
-    for i in range(firstLen, n):
-        currSum += nums[i] - nums[i - firstLen]
-        maxSumFirst[i] = currSum
-
-    # Precompute maximum sum of subarrays of length secondLen
-    currSum = sum(nums[:secondLen])
-    maxSumSecond[secondLen - 1] = currSum
-    for i in range(secondLen, n):
-        currSum += nums[i] - nums[i - secondLen]
-        maxSumSecond[i] = currSum
-
-    maxSum = 0
-    # Calculate the maximum sum considering non-overlapping subarrays
-    for i in range(n):
-        if i >= firstLen - 1:
-            maxSum = max(maxSum, maxSumFirst[i] + maxSumSecond[i - firstLen])
-        if i >= secondLen - 1:
-            maxSum = max(maxSum, maxSumFirst[i - secondLen] + maxSumSecond[i])
-
-    return maxSum
-
-
-def maxSumTwoNoOverlap2(nums, firstLen, secondLen):
-    n = len(nums)
-    maxSumFirst = [0] * n
-    maxSumSecond = [0] * n
-
-    # Precompute maximum sum of subarrays of length firstLen
-    currSum = sum(nums[:firstLen])
-    maxSumFirst[firstLen - 1] = currSum
-    for i in range(firstLen, n):
-        currSum += nums[i] - nums[i - firstLen]
-        maxSumFirst[i] = max(currSum, maxSumFirst[i - 1])
-
-    # Precompute maximum sum of subarrays of length secondLen
-    currSum = sum(nums[:secondLen])
-    maxSumSecond[secondLen - 1] = currSum
-    for i in range(secondLen, n):
-        currSum += nums[i] - nums[i - secondLen]
-        maxSumSecond[i] = max(currSum, maxSumSecond[i - 1])
-
-    maxSum = 0
-    # Calculate the maximum sum considering non-overlapping subarrays
-    for i in range(n):
-        if i >= firstLen - 1:
-            maxSum = max(maxSum, maxSumFirst[i] + maxSumSecond[i - firstLen + 1])
-        if i >= secondLen - 1:
-            maxSum = max(maxSum, maxSumFirst[i - secondLen + 1] + maxSumSecond[i])
-
-    return maxSum
-
-
-def maxSumTwoNoOverlap3(nums, firstLen, secondLen):  # sliding w
+def maxSumTwoNoOverlap(nums, firstLen, secondLen):  # sliding w On time O1 space
     def maxSum(L: int, M: int) -> int:
         sumL = sumM = 0
         for i in range(0, L + M):
@@ -77,7 +17,7 @@ def maxSumTwoNoOverlap3(nums, firstLen, secondLen):  # sliding w
     return max(maxSum(firstLen, secondLen), maxSum(secondLen, firstLen))
 
 
-def maxSumTwoNoOverlap4(nums, firstLen, secondLen):  # prefix sum
+def maxSumTwoNoOverlap2(nums, firstLen, secondLen):  # prefix sum On time/space
     def maxSum(L: int, M: int) -> int:
         maxL = ans = 0
         for i in range(L + M, len(prefixSum)):
@@ -93,15 +33,10 @@ def maxSumTwoNoOverlap4(nums, firstLen, secondLen):  # prefix sum
 
 nums = [0, 6, 5, 2, 2, 5, 1, 9, 4]
 nums2 = [1, 3, 3, 5, 4, 3, 1]
-print('maxSumTwoNoOverlap')
-print(maxSumTwoNoOverlap(nums2, 3, 2))  # output 18
-print(maxSumTwoNoOverlap(nums, 1, 2))  # Output has to be 20 but its 15
-print('maxSumTwoNoOverlap2')
-print(maxSumTwoNoOverlap2(nums2, 3, 2))  # output 18
-print(maxSumTwoNoOverlap2(nums, 1, 2))  # Output  20
+
 print('maxSumTwoNoOverlap3')
-print(maxSumTwoNoOverlap3(nums2, 3, 2))  # output 18
-print(maxSumTwoNoOverlap3(nums, 1, 2))  # Output 20
+print(maxSumTwoNoOverlap(nums2, 3, 2))  # output 18
+print(maxSumTwoNoOverlap(nums, 1, 2))  # Output 20
 print('maxSumTwoNoOverlap4')
-print(maxSumTwoNoOverlap4(nums2, 3, 2))  # output 18
-print(maxSumTwoNoOverlap4(nums, 1, 2))  # Output 20
+print(maxSumTwoNoOverlap2(nums2, 3, 2))  # output 18
+print(maxSumTwoNoOverlap2(nums, 1, 2))  # Output 20
