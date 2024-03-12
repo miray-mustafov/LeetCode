afc21480df2a3e4f9e4e1e29fdd4420079dafc1e
miray-mustafov <mustafovmiray@gmail.com>
2024-03-12 18:13:00 +0200

    153. Find Minimum in Rotated Sorted Array

---
+++ 15.Binary Search/03.153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,19 @@
+def solution(nums):
+    l, r = 0, len(nums) - 1
+    res = nums[0]
+    while l <= r:
+        if nums[l] < nums[r]:
+            res = min(res, nums[l])
+            break
+
+        m = l + ((r - l) // 2)
+        res = min(res, nums[m])
+        if nums[m] >= nums[l]:
+            l = m + 1
+        else:
+            r = m - 1
+    return res
+
+
+nums = [4, 5, 0, 1, 2, 3]
+print(solution(nums))
