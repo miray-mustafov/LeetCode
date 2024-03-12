7b9f5f98e4094bb185822d0fd39943b596e52277
miray-mustafov <mustafovmiray@gmail.com>
2024-03-12 14:45:50 +0200

    875. Koko Eating Bananas the nlogn sol

--- 15.Binary Search/02.875. Koko Eating Bananas.py
+++ 15.Binary Search/02.875. Koko Eating Bananas.py
@@ -1,6 +1,24 @@
 import math
 
 
+def binary_search_solution(piles, h):
+    l, r = 1, max(piles)
+    res = r
+    while l <= r:
+        k = l + ((r - l) // 2)
+
+        cur_h = 0
+        for p in piles:
+            cur_h += math.ceil(p / k)
+
+        if cur_h <= h:
+            res = min(res, k)
+            r = k - 1
+        else:
+            l = k + 1
+    return res
+
+
 def brute_force(piles, h):
     piles.sort()
     for bananas in range(1, piles[-1] + 1):
@@ -17,3 +35,7 @@ piles3, h3 = [30, 11, 23, 4, 20], 6  # 23
 print(brute_force(piles, h))
 print(brute_force(piles2, h2))
 print(brute_force(piles3, h3))
+print()
+print(binary_search_solution(piles, h))
+print(binary_search_solution(piles2, h2))
+print(binary_search_solution(piles3, h3))
