1faced49c203fa047115aba783b59f63df5b6bc7
miray-mustafov <mustafovmiray@gmail.com>
2024-03-18 11:02:34 +0200

    235. Lowest Common Ancestor of a Binary Search Tree

--- 09.Binary Tree General/neetcodeio/05.572. Subtree of Another Tree.py
+++ 09.Binary Tree General/neetcodeio/05.572. Subtree of Another Tree.py
@@ -29,9 +29,36 @@ def solution(root, subroot):
     return is_sub_tree
 
 
+def tutorial_solution(root, subRoot):
+    def dfs(node):
+        if not node:
+            return False
+        if node.val == subRoot.val and isSameTree(node, subRoot):
+            return True
+        leftt = dfs(node.left)
+        rightt = dfs(node.right)
+        res = leftt or rightt
+        return res
+
+    def isSameTree(p, q):
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+    if not subRoot:
+        return True
+    return dfs(root)
+
+
 root = make_tree_from_list([3, 4, 5, 1, 2])
 subRoot = make_tree_from_list([4, 1, 2])
 print(solution(root, subRoot))
+print(tutorial_solution(root, subRoot))
+
+print()
 root = make_tree_from_list([3, 4, 5, 1, 2, None, None, None, None, 0])
 subRoot = make_tree_from_list([4, 1, 2])
 print(solution(root, subRoot))
+print(tutorial_solution(root, subRoot))
---
+++ 09.Binary Tree General/neetcodeio/06.235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,9 @@
+def solution(root, p, q):
+    cur = root
+    while True:
+        if p.val > cur.val and q.val > cur.val:
+            cur = cur.right
+        elif p.val < cur.val and q.val < cur.val:
+            cur = cur.left
+        else:
+            return cur
