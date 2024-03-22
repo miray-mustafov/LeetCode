670dde491d2c2ab17445eca9d24567cae46c12b6
miray-mustafov <mustafovmiray@gmail.com>
2024-03-22 10:47:31 +0200

    46. Permutations neetcodes solution

---
+++ 13.Backtracking/02.46. Permutations.py
@@ -0,0 +1,23 @@
+def permute(nums):
+    res = []
+    if len(nums) == 1:
+        return [nums[:]]
+
+    for i in range(len(nums)):
+        n = nums.pop(0)
+        perms = permute(nums)
+
+        for perm in perms:
+            perm.append(n)
+        res.extend(perms)
+        nums.append(n)
+
+    return res
+
+
+nums = [1, 2, 3]
+actual = permute(nums)
+actual.sort()
+expected = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
+print(actual == expected)
