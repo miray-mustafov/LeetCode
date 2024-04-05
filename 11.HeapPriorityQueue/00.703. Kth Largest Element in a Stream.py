6addecab3ccff8d47ae741ffeb514aa8ef05715f
miray-mustafov <mustafovmiray@gmail.com>
2024-04-05 14:14:05 +0300

    703. Kth Largest Element in a Stream

---
+++ 11.HeapPriorityQueue/00.703. Kth Largest Element in a Stream.py
@@ -0,0 +1,76 @@
+import heapq
+
+from utils.tree.visualize import view_tree, make_tree_from_list
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+def left(i):
+    return 2 * i
+
+
+def right(i):
+    return 2 * i + 1
+
+
+def parent(i):
+    return i // 2
+
+
+def max_heapify(arr, heap_size, i):
+    l = left(i)
+    r = right(i)
+
+    largest = i
+
+    if l < heap_size and arr[l] > arr[i]:
+        largest = l
+
+    if r < heap_size and arr[r] > arr[largest]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        max_heapify(arr, heap_size, largest)
+
+
+def build_max_heap(arr):
+    heap_size = len(arr)
+
+    for i in range(heap_size // 2, 0, -1):
+        max_heapify(arr, heap_size, i)
+
+
+
+def main():
+    # root is at index 1
+    # it can be at index zero but see here: https://www.quora.com/Why-do-indexes-for-heaps-start-at-1
+    # and: https://stackoverflow.com/questions/22900388/why-in-a-heap-implemented-by-array-the-index-0-is-left-unused
+
+    arr = [None, 0, 5, 20, 6, 12, 65, 1, 4, 9, 3, 89, 22, 25, 28, 10]
+    build_max_heap(arr)
+
+    # root = make_tree_from_list(arr[1:])
+    # view_tree(root)
+
+    print(f'Heap: {arr[1:]}')
+
+
+main()
--- utils/tree/visualize.py
+++ utils/tree/visualize.py
@@ -1,5 +1,6 @@
 from graphviz import Digraph
 from utils.tree.tree import TreeNode
+from collections import deque
 
 
 def view_tree(root_node: TreeNode):
@@ -21,3 +22,25 @@ def view_tree(root_node: TreeNode):
 
     draw_node(root_node)
     dot.render(view=True)
+
+
+def make_tree_from_list(l, stringified=int):
+    if not l:
+        return ValueError('List empty!')  # [1,2,3,4,5]
+    n = len(l)
+    root = TreeNode(stringified(l[0]))
+    queue = deque([root])
+
+    i = 0
+    while queue:
+        node = queue.popleft()
+        lidx = i * 2 + 1
+        ridx = i * 2 + 2
+        if lidx < n and l[lidx] is not None:
+            node.left = TreeNode(stringified(l[lidx]))
+            queue.append(node.left)
+        if ridx < n and l[ridx] is not None:
+            node.right = TreeNode(stringified(l[ridx]))
+            queue.append(node.right)
+        i += 1
+    return root
