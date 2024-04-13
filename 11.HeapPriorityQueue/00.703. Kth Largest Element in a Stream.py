b03f61b6d51740a49d3b61d3a7a7dcf95757af98
miray-mustafov <mustafovmiray@gmail.com>
2024-04-13 12:54:31 +0300

    heap practice

--- 11.HeapPriorityQueue/00.703. Kth Largest Element in a Stream.py
+++ 11.HeapPriorityQueue/00.703. Kth Largest Element in a Stream.py
@@ -1,5 +1,5 @@
 import heapq
-
+from abc import abstractmethod
 from utils.tree.tree import view_tree
 
 
@@ -7,7 +7,9 @@ class KthLargest:
 
     def __init__(self, k: int, nums):
         self.minHeap, self.k = nums, k
+        view_tree(nums)
         heapq.heapify(self.minHeap)
+        view_tree(self.minHeap)
         while len(self.minHeap) > k:
             heapq.heappop(self.minHeap)
 
@@ -18,57 +20,72 @@ class KthLargest:
         return self.minHeap[0]
 
 
+# obj = KthLargest(3, [2, 9, 3, 5, 4])
+# while True:
+#     inp = input()
+#     if inp == 'end':
+#         break
+#     res = obj.add(int(inp))
+#     print(res)
+
+
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
 
-def left(i):
-    return 2 * i
+class MyMaxHeapClass:
+    def __init__(self, arr):
+        self.heap = arr
+        self.build_max_heap()
+
+    @staticmethod
+    def left(i):
+        return 2 * i
+
+    @staticmethod
+    def right(i):
+        return 2 * i + 1
+
+    @staticmethod
+    def parent(i):
+        return i // 2
+
+    def max_heapify(self, i):
+        l = self.left(i)
+        r = self.right(i)
+
+        largest = i
+
+        if l < len(self.heap) and self.heap[l] > self.heap[i]:
+            largest = l
+
+        if r < len(self.heap) and self.heap[r] > self.heap[largest]:
+            largest = r
+
+        if largest != i:
+            self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
+            self.max_heapify(largest)
+
+    def max_heap_add(self, num):
+        self.heap.append(num)
+        parent_i = self.parent(len(self.heap) - 1)
+        self.max_heapify(parent_i)
+
+    def build_max_heap(self):
+        for i in range(len(self.heap) // 2, 0, -1):
+            self.max_heapify(i)
 
 
-def right(i):
-    return 2 * i + 1
-
-
-def parent(i):
-    return i // 2
-
-
-def max_heapify(arr, heap_size, i):
-    l = left(i)
-    r = right(i)
-
-    largest = i
-
-    if l < heap_size and arr[l] > arr[i]:
-        largest = l
-
-    if r < heap_size and arr[r] > arr[largest]:
-        largest = r
-
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        max_heapify(arr, heap_size, largest)
-
-
-def build_max_heap(arr):
-    heap_size = len(arr)
-
-    for i in range(heap_size // 2, 0, -1):
-        max_heapify(arr, heap_size, i)
-
-
-def main():
+def main_implementation():
     # root is at index 1
     # it can be at index zero but see here: https://www.quora.com/Why-do-indexes-for-heaps-start-at-1
     # and: https://stackoverflow.com/questions/22900388/why-in-a-heap-implemented-by-array-the-index-0-is-left-unused
 
     arr = [None, 0, 5, 20, 6, 12, 65, 1, 4, 9, 3, 89, 22, 25, 28, 10]
-    build_max_heap(arr)
+    my_heap = MyMaxHeapClass(arr)
+    the_heap = my_heap.heap
+    # view_tree(the_heap[1:])
+    print(f'Heap: {the_heap[1:]}')
+    #todo vij adda sus graphiz
 
-    # view_tree(arr[1:])
-
-    print(f'Heap: {arr[1:]}')
-
-
-main()
+main_implementation()
