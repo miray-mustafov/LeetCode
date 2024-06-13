4ee48bfde103bdf685cd33341aff09e033cced7b
miray-mustafov <mustafovmiray@gmail.com>
2024-06-13 16:24:53 +0300

    heap practicee

--- 11.HeapPriorityQueue/01.1046. Last Stone Weight.py
+++ 11.HeapPriorityQueue/01.1046. Last Stone Weight.py
@@ -1,14 +1,32 @@
-import heapq
+def lastStoneWeight(stones):  # O (n logn)
+    import heapq as h
+    stones = [-n for n in stones]  # On
+    h.heapify(stones)  # On
 
-
-def f(stones):
-    stones = [-s for s in stones]
-    heapq.heapify(stones)
-
-    while len(stones) > 1:
-        first = heapq.heappop(stones)
-        second = heapq.heappop(stones)
-        heapq.heappush(stones, first - second)
-
+    while len(stones) > 1:  # On
+        x = h.heappop(stones)  # Ologn
+        y = h.heappop(stones)  # Ologn
+        res_stone = - abs(x - y)  # O1
+        if res_stone != 0:  # O1
+            h.heappush(stones, res_stone)  # Ologn
     stones.append(0)
-    return abs(stones[0])
+    return -stones[0]
+
+
+stones = [2, 7, 4, 1, 8, 1]
+print(lastStoneWeight(stones))
+
+# import heapq
+#
+#
+# def f(stones):
+#     stones = [-s for s in stones]
+#     heapq.heapify(stones)
+#
+#     while len(stones) > 1:
+#         first = heapq.heappop(stones)
+#         second = heapq.heappop(stones)
+#         heapq.heappush(stones, first - second)
+#
+#     stones.append(0)
+#     return abs(stones[0])
