904e6be1b7fcc1dfb6925748cb4692db88efabae
miray-mustafov <mustafovmiray@gmail.com>
2024-06-17 21:46:38 +0300

    215. Kth Largest Element in an Array with sorting

--- 11.HeapPriorityQueue/04.215. Kth Largest Element in an Array.py
+++ 11.HeapPriorityQueue/04.215. Kth Largest Element in an Array.py
@@ -1,3 +1,4 @@
+# with heap
 def findKthLargest(nums, k):
     import heapq as h
     for i in range(len(nums)):
@@ -10,7 +11,15 @@ def findKthLargest(nums, k):
     return -h.heappop(nums)
 
 
+# with sorting
+def f(nums, k):
+    nums.sort(reverse=True)
+    return nums[k-1]
+
+
 nums, k = [3, 2, 1, 5, 6, 4], 2
-print(findKthLargest(nums, k))
+print(findKthLargest(nums.copy(), k))
+print(f(nums.copy(), k))
 nums, k = [3, 2, 3, 1, 2, 4, 5, 5, 6], 4
-print(findKthLargest(nums, k))
+print(findKthLargest(nums.copy(), k))
+print(f(nums.copy(), k))
