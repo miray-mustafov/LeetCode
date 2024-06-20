732e9a67fdd4e3315f360d65fbae719def4dde64
miray-mustafov <mustafovmiray@gmail.com>
2024-06-20 13:51:58 +0300

    06.355. Design Twitter: Heap solution

--- 11.HeapPriorityQueue/06.355. Design Twitter.py
+++ 11.HeapPriorityQueue/06.355. Design Twitter.py
@@ -1,5 +1,6 @@
 # hard
 
+
 class Twitter:
     fake_time = -1
 
@@ -8,12 +9,12 @@ class Twitter:
         # self.tweets: dict[int, set[(int, int)]] = dict()  # dict(userId: set[tuples[tweetId,time]])
         self.tweets = []  # stack
 
-    def postTweet(self, userId: int, tweetId: int) -> None:
+    def postTweet(self, userId: int, tweetId: int) -> None:  # O1
         # Twitter.fake_time += 1
         # self.tweets[userId].add((tweetId, Twitter.fake_time))
         self.tweets.append((userId, tweetId))
 
-    def getNewsFeed(self, userId: int) -> list[int]:
+    def getNewsFeed(self, userId: int) -> list[int]:  # On
         feed = []
         for i in range(len(self.tweets) - 1, -1, -1):
             cur_userId = self.tweets[i][0]
@@ -24,13 +25,13 @@ class Twitter:
                 break
         return feed
 
-    def follow(self, followerId: int, followeeId: int) -> None:
+    def follow(self, followerId: int, followeeId: int) -> None:  # O1
         if not self.followings.get(followerId):
             self.followings[followerId] = set()
 
         self.followings[followerId].add(followeeId)
 
-    def unfollow(self, followerId: int, followeeId: int) -> None:
+    def unfollow(self, followerId: int, followeeId: int) -> None:  # O1
         if not self.followings.get(followerId) or followeeId not in self.followings[followerId]:
             return  # no such following
 
@@ -54,12 +55,62 @@ obj.unfollow(user1, user2)
 
 print(obj.getNewsFeed(user1))
 
+from collections import defaultdict
+import heapq as h
 
 
-# operations = {
-#     'Twitter': Twitter,
-#     'postTweet': self.postTweet,
-#     'getNewsFeed': Twitter,
-#     'follow': Twitter,
-#     'unfollow': Twitter,
-# }
+class Twitter2:
+
+    def __init__(self):
+        self.count = 0
+        self.tweetMap = defaultdict(list)
+        self.followMap = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:  # O1
+        self.tweetMap[userId].append((self.count, tweetId))
+        self.count -= 1
+
+    def getNewsFeed(self, userId: int) -> list[int]:  # O
+        res, minHeap = [], []
+
+        self.followMap[userId].add(userId)  # make user follow himself for easiness
+        # go greedy building a list with the last tweets of each followee
+        for followeeId in self.followMap[userId]:  # Om  (m = follower's followees)
+            if followeeId not in self.tweetMap:
+                continue
+            index = len(self.tweetMap[followeeId]) - 1
+            count, tweetId = self.tweetMap[followeeId][index]
+            minHeap.append((count, tweetId, followeeId, index - 1))
+
+        h.heapify(minHeap)  # Omlogm
+
+        while minHeap and len(res) < 10:  # O 10 logm
+            count, tweetId, followeeId, index = h.heappop(minHeap)  # Ologm
+            res.append(tweetId)
+            if index >= 0:
+                count, tweetId = self.tweetMap[followeeId][index]
+                h.heappush(minHeap, (count, tweetId, followeeId, index - 1))  # Ologm
+
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:  # O1
+        self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:  # O1
+        if followeeId in self.followMap.get(followerId, []):
+            self.followMap[followerId].remove(followeeId)
+
+
+user1 = 1
+user2 = 2
+
+obj2 = Twitter2()
+obj2.postTweet(user1, 5)
+print(obj2.getNewsFeed(user1))
+obj2.follow(user1, user2)
+
+obj2.postTweet(user2, 6)
+print(obj2.getNewsFeed(user1))
+obj2.unfollow(user1, user2)
+
+print(obj2.getNewsFeed(user1))
