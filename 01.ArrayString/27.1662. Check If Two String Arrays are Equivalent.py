d6593d9c294766c3c867fc810e95b0765f013dc3
miray-mustafov <mustafovmiray@gmail.com>
2024-06-28 18:39:14 +0300

    1662. Check If Two String Arrays are Equivalent my sol

---
+++ 01.ArrayString/27.1662. Check If Two String Arrays are Equivalent.py
@@ -0,0 +1,29 @@
+def f(word1, word2):
+    idx1 = idx2 = 0
+    i1 = i2 = 0
+    while idx1 < len(word1) and idx2 < len(word2):
+        w1, w2 = word1[idx1], word2[idx2]
+
+        while i1 < len(w1) and i2 < len(w2):
+            if w1[i1] != w2[i2]:
+                return False
+            i1, i2 = i1 + 1, i2 + 1
+
+        if not i1 < len(w1):
+            i1, idx1 = 0, idx1 + 1
+        if not i2 < len(w2):
+            i2, idx2 = 0, idx2 + 1
+
+    if i1 != 0 or i2 != 0:  # the edge case, means one is longer
+        return False
+    return True
+
+
+word1, word2 = ["ab", "c"], ["a", "bc"]
+print(f(word1, word2))
+word1, word2 = ["a", "cb"], ["ab", "c"]
+print(f(word1, word2))
+word1, word2 = ["a", "b", 'c'], ["abc"]
+print(f(word1, word2))  # True
+word1, word2 = ["a", "b", 'cc'], ["abc"]  # the edge case
+print(f(word1, word2))  # False
