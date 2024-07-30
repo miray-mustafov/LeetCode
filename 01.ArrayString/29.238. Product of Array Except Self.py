e488a5a5fa67cb584a1382e8e078c7a427cd34ce
miray-mustafov <mustafovmiray@gmail.com>
2024-07-30 16:49:43 +0300

    tutorial

--- 01.ArrayString/29.238. Product of Array Except Self.py
+++ 01.ArrayString/29.238. Product of Array Except Self.py
@@ -1,6 +1,15 @@
 def tutorial(nums):  # O1 space
-    pass
+    res = [1] * len(nums)
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
 
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+    return res
 
 def noDivisionSolution(nums):  # On memory and runtime
     output = []
