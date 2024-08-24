05da5aaa48df094e986d70b6a5646654d4376008
miray-mustafov <mustafovmiray@gmail.com>
2024-08-24 10:21:34 +0300

    739. Daily Temperatures tutorial implementation

--- 07.Stack/06.739. Daily Temperatures.py
+++ 07.Stack/06.739. Daily Temperatures.py
@@ -1,15 +1,13 @@
 def f(temperatures):
-    if len(temperatures) == 1:
-        return [0]
-
     answer = [0] * len(temperatures)
-    stack = [(temperatures[0], 0)]
-    for i, t in enumerate(temperatures[1:], start=1):
+    stack = []
+    for i, t in enumerate(temperatures):
         while stack and t > stack[-1][0]:
             cur = stack.pop()
             answer[cur[1]] = i - cur[1]
 
         stack.append((t, i))
+
     return answer
 
 
